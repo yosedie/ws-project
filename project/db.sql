f921006dfc8ebbd0ac1bd449f65cff820b7ab503
Richcie <35892425+richcie@users.noreply.github.com>
2024-05-03 18:12:39 +0700

    push database

--- project/db.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE `ws_project`;
-USE `ws_project`;
--- project/project_ws.sql
+++ project/project_ws.sql
@@ -39,7 +39,46 @@ CREATE TABLE `owner` (
   `no_telepon` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `H_trans`
+--
+
+DROP TABLE IF EXISTS `H_trans`;
+CREATE TABLE `H_trans` (
+  `trans_id` int(11) NOT NULL AUTO_INCREMENT,
+  `restaurant_id` int(11) NOT NULL,
+  `status_transaksi` ENUM('Berhasil', 'Gagal') NOT NULL,
+  PRIMARY KEY (`trans_id`),
+  FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`restaurant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `D_trans`
+--
+
+DROP TABLE IF EXISTS `D_trans`;
+CREATE TABLE `D_trans` (
+  `d_trans_id` int(11) NOT NULL AUTO_INCREMENT,
+  `trans_id` int(11) NOT NULL,
+  `detail_transaksi` text NOT NULL,
+  PRIMARY KEY (`d_trans_id`),
+  FOREIGN KEY (`trans_id`) REFERENCES `H_trans`(`trans_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `owner`
+--
+
+DROP TABLE IF EXISTS `owner`;
+CREATE TABLE `owner` (
+  `owner_id` INT AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `name` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `no_telepon` INT NOT NULL,
+  PRIMARY KEY (`owner_id`)
+);
 
 --
 -- Table structure for table `restaurant`
@@ -51,7 +90,77 @@ CREATE TABLE `restaurant` (
   `nama_restaurant` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `deskripsi` text NOT NULL,
-  `owner_id` int(11) NOT NULL
+  `owner_id` int(11) NOT NULL,
+  PRIMARY KEY (`restaurant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `item`
+--
+
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE `item` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_item` varchar(255) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `bahan` text NOT NULL,
+  `quantitas` decimal(10,2) NOT NULL,
+  `satuan` varchar(50) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`restaurant_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `menu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_menu` varchar(255) NOT NULL,
+  `restaurant_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  PRIMARY KEY (`menu_id`),
+  FOREIGN KEY (`restaurant_id`) REFERENCES `restaurant`(`restaurant_id`),
+  FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `menu_item`
+--
+
+DROP TABLE IF EXISTS `menu_item`;
+CREATE TABLE `menu_item` (
+  `menu_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `needed_quantity` decimal(10,2) NOT NULL,
+  FOREIGN KEY (`menu_id`) REFERENCES `menu`(`menu_id`),
+  FOREIGN KEY (`item_id`) REFERENCES `item`(`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+  `member_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_member` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`member_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Table structure for table `kupon`
+--
+
+DROP TABLE IF EXISTS `kupon`;
+CREATE TABLE `kupon` (
+  `kupon_id` int(11) NOT NULL AUTO_INCREMENT,
+  `member_id` int(11) NOT NULL,
+  `potongan` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`kupon_id`),
+  FOREIGN KEY (`member_id`) REFERENCES `member`(`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
